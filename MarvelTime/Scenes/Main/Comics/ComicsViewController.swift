8f079a7a548bbc7590fa11a22ed4707ceff9cd73
Batuhan Kacmaz <batuhan.kacmaz1105@gmail.com>
2023-04-25 00:21:21 +0300

    create comicTableView

--- MarvelTime/Scenes/Main/Comics/ComicsViewController.swift
+++ MarvelTime/Scenes/Main/Comics/ComicsViewController.swift
@@ -12,14 +12,35 @@ class ComicsViewController: UIViewController {
     var didSendEventClosure: ((ComicsViewController.Event) -> Void)?
     
     private let navigationBarTitle = MTNavigationBarTitle(frame: CGRect(x: 0, y: 0, width: 120, height: 30))
+    
     private let navigationBarAvatar = MTNavigationBarAvatar(frame: CGRect(x: 0, y: 0, width: 32, height: 32), avatarName: "wolverine", size: 32)
+    
+    private let comicsTable: UITableView = {
+       
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(MTComicTableViewCell.self, forCellReuseIdentifier: MTComicTableViewCell.identifier)
+        table.backgroundView = nil
+        table.backgroundColor = .clear
+        return table
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor(named: "DarkGray")
         
+        comicsTable.delegate = self
+        comicsTable.dataSource = self
+        
+        view.addSubview(comicsTable)
+        
         configureUI()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        comicsTable.frame = view.bounds
     }
     
     private func configureUI() {
@@ -52,4 +73,31 @@ extension ComicsViewController {
     enum Event {
         case favorites
     }
+}
+
+//MARK: - UITableViewDataSource
+
+extension ComicsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: MTComicTableViewCell.identifier, for: indexPath)
+        cell.backgroundColor = .clear
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+
+    
+    
+}
+
+//MARK: - UITableViewDelegate
+
+extension ComicsViewController: UITableViewDelegate {
+    
 }
--- MarvelTime/Views/MTComicTableViewCell.swift
+++ MarvelTime/Views/MTComicTableViewCell.swift
@@ -8,16 +8,13 @@
 import UIKit
 
 class MTComicTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    static let identifier = "MTComicTableViewCell"
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 0, left: 0, bottom: 10, right: 0))
+        contentView.backgroundColor = .blue
     }
 
 }
