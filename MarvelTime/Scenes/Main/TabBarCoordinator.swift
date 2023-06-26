7d560816686f6c1e22c0d8e2845cf9a1f8eda82b
Batuhan Kacmaz <batuhan.kacmaz@wunder.is>
2023-06-26 22:31:24 +0300

    add colors resolve packages

--- MarvelTime/Scenes/Main/TabBarCoordinator.swift
+++ MarvelTime/Scenes/Main/TabBarCoordinator.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class TabBarCoordinator: NSObject, Coordinator {
+    var rootViewController: UIViewController?
+    
     weak var finishDelegate: CoordinatorFinishDelegate?
     
     var childCoordinators = [Coordinator]()
