970ccd2818b263d80bf12cbe7f517a88929e4419
Batuhan Kacmaz <batuhan.kacmaz@wunder.is>
2023-06-26 22:28:16 +0300

    add static enum & change MTTextfield add viewmodel & error label

--- MarvelTime/Scenes/Login/LoginViewController.swift
+++ MarvelTime/Scenes/Login/LoginViewController.swift
@@ -7,9 +7,35 @@
 
 import UIKit
 
+enum Icon: String {
+    case username = "person.fill"
+    case password = "lock.fill"
+    
+    func toImage() -> UIImage {
+        return UIImage(systemName: self.rawValue)!
+    }
+}
+
+enum PlaceHolder: String {
+    case username = "Enter your username"
+    case password = "Enter your password"
+    case repassword = "Enter your password again"
+}
+
 class LoginViewController: UIViewController {
     
     var didSendEventClosure: ((LoginViewController.Event) -> Void)?
+    private var isLoginButtonActive = false {
+        didSet {
+            loginButton.backgroundColor = isLoginButtonActive ? UIColor(named: "DarkYellow") : .gray
+            loginButton.isEnabled = isLoginButtonActive
+        }
+    }
+    private var viewModel: LoginViewModelProtocol! {
+        didSet {
+            viewModel.delegate = self
+        }
+    }
     
     private let avatarView: UIImageView = {
         
@@ -19,30 +45,25 @@ class LoginViewController: UIViewController {
         return image
     }()
     
-    private let userName: MTIconWithTextField = {
-        
-        let viewModel = MTIconWithTextFieldViewModel(icon: "person.fill", placeholder: "Enter your username")
-        let textField = MTIconWithTextField()
-        textField.configure(with: viewModel)
-      
-        
-        return textField
+    
+    private let userName = MTIconWithTextField(icon: Icon.username.toImage(), placeholder: PlaceHolder.username.rawValue)
+    
+    private let password = MTIconWithTextField(icon: Icon.password.toImage(), placeholder: PlaceHolder.password.rawValue, isPassword: true)
+    
+    private let errorLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Wrong username or password"
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = UIColor(named: "Red")
+        label.layer.opacity = 0
+        return label
     }()
-    private let password: MTIconWithTextField = {
-        
-        let viewModel = MTIconWithTextFieldViewModel(icon: "lock.fill", placeholder: "Enter your password", isPassword: true)
-        let textField = MTIconWithTextField()
-        textField.configure(with: viewModel)
-   
-        
-        return textField
-    }()
-
+    
     private let loginButton: UIButton = {
         
         let button = UIButton()
         button.setTitle("Login", for: .normal)
-        button.backgroundColor = UIColor(named: "DarkYellow")
+        button.backgroundColor = .gray
         button.setTitleColor(.white, for: .normal)
         button.layer.cornerRadius = 8.0
         
@@ -56,24 +77,21 @@ class LoginViewController: UIViewController {
         button.setTitle("Don't have an account. Register", for: .normal)
         button.setTitleColor(UIColor(named: "DarkYellow"), for: .normal)
         button.backgroundColor = .clear
-       
-      
         
         return button
     }()
     
     lazy var textFieldStack: UIStackView = {
-       let stackView = UIStackView()
+        let stackView = UIStackView()
         stackView.axis = .vertical
-        stackView.spacing = 40.0
-        stackView.alignment = .fill
-        stackView.distribution = .fillEqually
-        stackView.addSubviews([userName, password])
+        stackView.spacing = 10
+        stackView.distribution = .equalCentering
+        stackView.addSubviews([userName, password, errorLabel])
         return stackView
     }()
     
     lazy var buttonStack: UIStackView = {
-       let stackView = UIStackView()
+        let stackView = UIStackView()
         stackView.axis = .vertical
         stackView.spacing = 20.0
         stackView.alignment = .fill
@@ -83,21 +101,19 @@ class LoginViewController: UIViewController {
     }()
     
     lazy var loginStack: UIStackView = {
-       let stackView = UIStackView()
+        let stackView = UIStackView()
         stackView.axis = .vertical
         stackView.spacing = 60.0
-        stackView.addSubviews([ avatarView, textFieldStack, buttonStack])
+        stackView.addSubviews([ avatarView,textFieldStack, buttonStack])
         return stackView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        viewModel = LoginViewModel()
         view.backgroundColor = UIColor(named: "DarkGray")
         self.navigationItem.backButtonTitle = "Login"
         self.hideKeyboardWhenTappedAround()
-        
-        
         
         configureUI()
         animatedAppear()
@@ -120,13 +136,14 @@ class LoginViewController: UIViewController {
         configureLoginStack()
         configureLoginButton()
         configureRegisterButton()
+        
     }
     
     private func initialLocation() {
         avatarView.center.x -= view.bounds.width
         textFieldStack.center.x -= view.bounds.width
         buttonStack.center.y += view.bounds.height
-    
+        
     }
     
     private func animatedAppear() {
@@ -150,9 +167,10 @@ class LoginViewController: UIViewController {
             loginStack.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             loginStack.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8),
             loginStack.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5),
-        
+            
         ])
     }
+    
     
     private func configureLoginButton() {
         loginButton.addTarget(self, action: #selector(didTapLoginButton), for: .touchUpInside)
@@ -163,8 +181,8 @@ class LoginViewController: UIViewController {
     }
     
     @objc private func didTapLoginButton() {
-        let loginEvent = Event.login
-        didSendEventClosure?(loginEvent)
+        viewModel.LoginUIEvents(.handleLogin)
+        
     }
     
     @objc private func didTapRegisterButton() {
@@ -185,20 +203,33 @@ extension LoginViewController {
 
 
 extension LoginViewController: MTIconWithTextFieldDelegate {
-    func textDidChange(text: String?) {
-        print(text ?? "")
+    func textDidChange(text: String, type: MTIconWithTextFieldType) {
+        print("textChange", text)
     }
     
-    func textFieldDidBeginEditing(textField: UITextField) {
-        print("Begin editing")
+    func textFieldDidEndEditing(text: String, type: MTIconWithTextFieldType) {
+        switch type {
+        case .username:
+            viewModel.LoginUIEvents(.handleUsername(username: text))
+        case .password:
+            viewModel.LoginUIEvents(.handlePassword(password: text))
+        }
     }
     
-    func textFieldDidEndEditing(textField: UITextField) {
-        print("DidEndEditing")
-    }
-    
-    func textField(textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) {
-        print("Textfield")
+}
+
+extension LoginViewController: LoginViewModelDelegate {
+    func LoginViewModelOutputs(_ outputEvent: LoginViewModelOutput) {
+        switch outputEvent {
+        case .success:
+            let loginEvent = Event.login
+            didSendEventClosure?(loginEvent)
+        case .hasError(let error):
+            errorLabel.layer.opacity = error ? 1.0 : 0
+        case .loginButtonActive(let isActive):
+            print("isActive", isActive)
+            isLoginButtonActive = isActive
+        }
     }
     
     
